f3cc0df4eb3cc2e6dc780806aa8cde85b86c3f7e
fabinQ <maciejfabinski@gmail.com>
2023-12-05 23:38:23 +0100

    Poprawienie funkcji __str__

--- mapper v2.py
+++ mapper v2.py
@@ -5,15 +5,7 @@ class File:
     def __init__(self, file_path):
         self.file_path = open(file_path, 'r')
         self.file_info = self.header()
-        self.version_SBG = self.file_info[0][0]
-        self.type_document = self.file_info[0][1]
-        self.coding = self.file_info[0][2]
-        self.application = self.file_info[1][1]
-        self.serialNo = self.file_info[2][1]
-        self.author = self.file_info[3][1]
-        self.company = self.file_info[4][1]
-        self.description = self.file_info[5][0]
-        self.coordinate_system = self.file_info[6][0]
+
 
     def __next__(self):
         return next(self.file_path).strip()
@@ -21,7 +13,7 @@ class File:
         return self
 
     def __str__(self):
-        return f"{self.version_SBG}-{self.type_document}-{self.coding}-{self.application} - {self.serialNo} - {self.author} - {self.company} - {self.description} - {self.coordinate_system}"
+        return '\n'.join([' - '.join(map(str, line)) for line in self.file_info])
 
 
     def header(self):
