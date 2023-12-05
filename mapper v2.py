d704fe11926649f45ce04f534ba5b3e1a2aa3702
fabinQ <maciejfabinski@gmail.com>
2023-12-05 20:05:56 +0100

    Usunięcie drugiej klasy i złączenie ją w jedną.

--- mapper v2.py
+++ mapper v2.py
@@ -5,11 +5,23 @@ class File:
     def __init__(self, file_path):
         self.file_path = open(file_path, 'r')
         self.file_info = self.header()
+        self.version_SBG = self.file_info[0][0]
+        self.type_document = self.file_info[0][1]
+        self.coding = self.file_info[0][2]
+        self.application = self.file_info[1][1]
+        self.serialNo = self.file_info[2][1]
+        self.author = self.file_info[3][1]
+        self.company = self.file_info[4][1]
+        self.description = self.file_info[5][0]
+        self.coordinate_system = self.file_info[6][0]
 
     def __next__(self):
         return next(self.file_path).strip()
     def __iter__(self):
         return self
+
+    def __str__(self):
+        return f"{self.version_SBG}-{self.type_document}-{self.coding}-{self.application} - {self.serialNo} - {self.author} - {self.company} - {self.description} - {self.coordinate_system}"
 
 
     def header(self):
@@ -25,27 +37,7 @@ class File:
         return file_info
 
 file = File('krawedzie.geo')
-
+print(str(file))
 
 # for line in file:
 #     print(line)
-
-
-class FileLine(File):
-    def __init__(self, file_path, file_info):
-        File.__init__(file_path,file_info)
-        self.version_SBG = file_info[0][0]
-        self.type_document = file_info[0][1]
-        self.coding = file_info[0][3]
-        self.application = file_info[1][1]
-        self.serialNo =file_info[2][1]
-        self.author = file_info[3][1]
-        self.company = file_info[4][1]
-        self.description = file_info[5][0]
-        self.coordinate_system = file_info[6][0]
-    def __str__(self):
-        print(f"{self.version_SBG}-{self.type_document}-{self.coding}-{self.application} - {self.serialNo} - {self.author} - {self.company} - {self.description} - {self.coordinate_system}")
-        return f"{self.version_SBG}-{self.type_document}-{self.coding}-{self.application} - {self.serialNo} - {self.author} - {self.company} - {self.description} - {self.coordinate_system}"
-
-
-print(str(FileLine))
