b17de19563d9bdb23b21605e81c59a514b2fb4e9
fabinQ <maciejfabinski@gmail.com>
2023-12-07 23:29:47 +0100

    Zmiana header na liste tupli

--- mapper v2.py
+++ mapper v2.py
@@ -1,32 +1,34 @@
 import os
+import json
+import sys
 
 
 class File:
     def __init__(self, file_path):
         self.file_path = open(file_path, 'r')
         self.file_info = self.header()
-
-
+        print(sys.getsizeof(self.file_info))
     def __next__(self):
         return next(self.file_path).strip()
+
     def __iter__(self):
         return self
 
     def __str__(self):
-        return '\n'.join([' - '.join(map(str, line)) for line in self.file_info])
-
+        return '\n'.join((' - '.join(map(str, line)) for line in self.file_info))
 
     def header(self):
         file_info = []
         header_line = next(self.file_path).split('"')[1::2]
-        file_info.append(header_line)
+        file_info.append(tuple(header_line))
         next(self.file_path)
         while header_line:
             header_line = next(self.file_path).split('"')[1::2]
-            file_info.append(header_line)
+            file_info.append(tuple(header_line))
         file_info.pop()
         print(file_info)
         return file_info
+
 
 file = File('krawedzie.geo')
 print(str(file))
