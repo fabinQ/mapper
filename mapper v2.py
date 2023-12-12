1b00270808645ea7d085f67d385d504107904bec
fabinQ <maciejfabinski@gmail.com>
2023-12-12 22:36:06 +0100

    Szukanie w pliku linijek odpowiedzialnych za nazwę linii oraz punkty

--- mapper v2.py
+++ mapper v2.py
@@ -2,13 +2,14 @@ import os
 import json
 import sys
 import datetime
+import re
 
 
 class File:
     def __init__(self, file_path):
         self.file_path = open(file_path, 'r', encoding='utf-8')
         self.file_info = self.header()
-        self.json_file()
+        # self.json_file()
 
     def __next__(self):
         return next(self.file_path).rstrip()
@@ -35,6 +36,8 @@ class File:
         return file_info
 
     def json_file(self):
+        if not os.path.exists('./json'):
+            os.mkdir('./json')
         file_name = os.path.join('./json/', self.formatted_datatime + ' ' + self.file_info.get('Company') + '.json')
         with open(file_name, "w", encoding="cp1250", errors="xmlcharrefreplace") as json_file:
             json.dump(self.file_info, json_file, indent=2)
@@ -50,16 +53,28 @@ class Line(File):
 
 
 # file = File('krawedzie.geo')
-# file = File('GRZ-25511-27300.geo')
+file = File('GRZ-25511-27300.geo')
 # file = File('1.geo')
-file = File('Chorzew_T5_spód_tłucznia.geo')
+# file = File('Chorzew_T5_spód_tłucznia.geo')
 # file = File('Chorzew_T5_spód_tłucznia v2.geo')
 # file = File('lk zegrze.geo')
 # file = File('ŚR i CH.geo')
 
 # print(str(file))
 
-for line in file:
-    # if line =='LineList':
-     print(line)
 
+line_pattern = re.compile(r'\tLine "(.+?)"(?:,(\d+|),)?,(.+)?')
+line_point_pattern = re.compile(r'\t\t\tPoint "(?P<Number>\d+)",(?P<X>\d+\.\d+),(?P<Y>\d+\.\d+)(?:,(?P<Z>\d+\.\d+))?(?:,"(?P<Code>.*?)",,)?')
+def generate_line_names_points(file, pattern):
+    pass
+
+for line in file:
+    print(line)
+    if line_pattern.match(line):
+        print(line_pattern.match(line))
+        for line in file:
+            print(line)
+            if line_point_pattern.match(line):
+                print(line_point_pattern.match(line))
+            elif line == '\tend':
+                break
