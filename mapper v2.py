3f253215dd9285f655ff77942a3d41d7a69927ac
fabinQ <maciejfabinski@gmail.com>
2023-12-26 01:04:20 +0100

    poprawa klasy Line oraz zmiany w sposobie zapisu nowych linii - zapisuje całość a nie częściami

--- mapper v2.py
+++ mapper v2.py
@@ -3,7 +3,7 @@ import json
 import sys
 import datetime
 import re
-
+import pprint
 
 class File:
     def __init__(self, file_path):
@@ -11,7 +11,7 @@ class File:
         self.file_path = open(file_path, 'r', encoding='utf-8')
         self.file_info = self.header()
         self.file_name = './json/' + self.data_time_stamp + ' ' + self.file_info.get('Company') + '.json'
-        # self.json_file(self.file_info, self.file_name)
+        self.json_file(self.file_info, self.file_name)
 
     def __next__(self):
         return next(self.file_path).rstrip()
@@ -56,16 +56,16 @@ class Line(File):
         file_name = file.file_name
         self.line_id_class = line_id_class
         self.line_point_class = line_point_class
-        self.line_class = {'Line': ({'Info': self.line_id_class, 'PointList': self.line_point_class})}
+        self.line_class = {'Info': self.line_id_class, 'PointList': self.line_point_class}
         # self.json_file(self.line_class, file_name)
         # number_of_line +=1
 
-    def simplifyer(self):
+    def simplifier(self):
         pass
 
 
-# file = File('krawedzie.geo')
-file = File('GRZ-25511-27300.geo')
+file = File('krawedzie.geo')
+# file = File('GRZ-25511-27300.geo')
 # file = File('krawedzie3.geo')
 # file = File('1.geo')
 # file = File('Chorzew_T5_spód_tłucznia.geo')
@@ -80,46 +80,42 @@ assert str(file).endswith('.geo')
 def generate_line_names_points():
     line_point = []
     for _ in file:
-        # print(_)
         if line_point_pattern.match(_):
             line_point_dic = line_point_pattern.match(_).groupdict()
-            # print(line_point_dic)
             line_point.append(line_point_dic)
         elif _ == '\tend':
             return line_point
 
 
+# patterny do linii i punktów
 line_pattern = re.compile(r'\tLine "(?P<ID_line>.+?)"?,(?P<Polygon>\d+|)?,(?P<Descriptoin>.+)?')
 line_point_pattern = re.compile(r'\t\t\tPoint(?: "(?P<Number>.+|)")?,(?P<X>\d+\.\d+|\d+),(?P<Y>\d+\.\d+|\d+),'
                                 r'(?P<Z>\d+\.\d+|-\d+\.\d+|\d+)?(?:,"(?P<Code>.*?)")?,?')
 
 index_of_line = 0
-dic_of_line_class = {}
+dic_of_line_class = []
 
 for line in file:
-    # print(line)
     if line_pattern.match(line):
         line_id = line_pattern.match(line).groupdict()
         line_points = generate_line_names_points()
+        pp = pprint.PrettyPrinter(width=41, compact=True)
 
-        # Tworzenie instancji klasy Line
+        #Tworzenie instancji klasy Line
         line_instance = Line(line_id, line_points)
-        # print(id(line_instance))
-        # print(vars(line_instance))
 
         # Przypisanie instancji do słownika pod unikalnym kluczem
-        dic_of_line_class['Line_' + str(index_of_line)] = line_instance
+        dic_of_line_class.append(line_instance)
         index_of_line += 1
-        # break
 
-
-# print(((list(dic_of_line_class.items()))[0])[1].line_point_class)
+lvl_of_simplifier = 2
 new_line = []
-file_name  = file.file_name.rstrip('.json') + "_new_lines.json"
-# print(file_name)
-for i in dic_of_line_class.values():
+new_line_info = []
+file_name = file.file_name.rstrip('.json') + "_new_lines.json"
+
+for i in dic_of_line_class:
     i= i.line_class
-    # print(i['Line']['Info'])
-    new_line = i['Line']['PointList'][0::10]
-    new_line_info= {'Line': ({'Info': i['Line']['Info'], 'PointList': new_line})}
-    File.json_file(new_line_info, file_name)
+    new_line = i['PointList'][0::lvl_of_simplifier]
+    new_line_info.append({'Info': i['Info'], 'PointList': new_line})
+    # File.json_file(new_line_info, file_name)
+pp.pprint(new_line_info)
