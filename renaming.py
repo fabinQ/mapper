5fad28002f4a0cef3ef6ddd11cfc503afcebf957
fabinQ <maciejfabinski@gmail.com>
2025-03-13 10:13:11 +0100

    ne tools to rename file

---
+++ renaming.py
@@ -0,0 +1,38 @@
+import os.path
+
+def patch_creator( catalog, file):
+    return os.path.join(catalog,file)
+def get_files(current_path, extension, recursive = False):
+    fitFiles = []
+
+    if (os.path.exists(current_path)):
+        if recursive:
+            for catalog, _, files in os.walk(current_path):
+                for file in files:
+                    if file.endswith(extension):
+                        fitFiles.append(patch_creator(catalog, file))
+        else:
+            for fileString in os.listdir(current_path):
+                file = patch_creator(current_path, fileString)
+                if os.path.isfile(file) and fileString.endswith(extension):
+                    fitFiles.append(file)
+    return fitFiles
+
+def rename_file( files ):
+    for file in files:
+        catalog, fileName = os.path.split(file)
+        newFileName = fileName.replace("%", "")
+        newFilePath = patch_creator(catalog, newFileName)
+
+        if newFileName != fileName:
+            os.rename(file, newFilePath)
+
+
+
+print("Podaj ścieżkę: ")
+# current_path = input()
+current_path = r"C:\OneDrive - 3D GEOSYSTEMY MICHAŁ JAŚKIEWICZ\Projekty iCON Office\KB CONSTRUCTION\LK 202 LĘBORK\PODFOLDERY\13.03.2025"
+extension = ".xml"
+
+fitFiles = get_files(current_path, extension, recursive = True)
+rename_file(fitFiles)
