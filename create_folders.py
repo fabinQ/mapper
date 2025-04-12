1914989eec03a5040248c760e2c77f4e8ff889f5
fabinQ <maciejfabinski@gmail.com>
2025-04-12 22:31:25 +0200

    refactoring, new tool

---
+++ create_folders.py
@@ -0,0 +1,14 @@
+import os
+
+path = input("Podaj ścieżke mordo\n")
+path = "C:\Users\Hyperbook\Desktop\Nowy folder"
+folders = input("Podaj nazwy folderów\n")
+folders = "GEO WLOKNINY 136,19\nGEO WLOKNINY 136,19 - Tri\nGEO WLOKNINY 136,64\nGEO WLOKNINY 136,69\nGEO WLOKNINY 137,14\nGEO WLOKNINY 137,19\nGEO WLOKNINY 137,64\nGEO WLOKNINY 137,84\nGEO WLOKNINY 138,10"
+
+folders = folders.split("\n")
+print(folders)
+for folder in folders:
+    print(folder)
+    pathFolder = os.path.join(path, folder)
+    os.mkdir(pathFolder)
+
--- file_name.py
+++ file_name.py
@@ -9,45 +9,39 @@ def scantree(path):
             yield i
 
 
+
+
 def main():
 
-    path = input("Podaj ścieżke mordo ")
+    # path = input("Podaj ścieżke mordo ")
+    path = r"C:\OneDrive - 3D GEOSYSTEMY MICHAŁ JAŚKIEWICZ\Projekty iCON Office\LEWCZUK\LK 201\Korytarze\17.01.2025"
     print(path)
-    extension_lin = '.lin'
-    extension_trm = '.trm'
-
-    # # a = glob.glob(path+'\\*'+extension_lin)
-    # # print(list(os.path.basename(i) for i in a))
-    # # for i in a:
-    # #     print(os.path.basename(i))
-    #
-    # files = []
-    # for filename_with_excetion in os.listdir(path):
-    #     filename, extension = os.path.splitext(filename_with_excetion)
-    #     if extension == extension_lin or extension == '.l3d':
-    #         print("Oś: ", filename)
-    #         files.append(filename_with_excetion)
-    #     elif extension == extension_trm:
-    #         print("Model: ",filename)
-    #         files.append(filename_with_excetion)
-    #
-    # print('\nW katalogu jest {} plików.'.format(len(files)))
-    # pyperclip.copy('\n'.join(files))
-
-    print(''*2)
+    extensionTrm = '.trm'
     files = []
+
     for i in scantree(path):
         files.append(i.name)
 
     files = sorted(files, key= lambda x: os.path.splitext(x))
-    sum_of_files = 0
-    for excention, file in it.groupby(files,key= lambda x: os.path.splitext(x)[1]):
-        file = list(file)
-        sum_of_files+=len(file)
-        print('{} = {}'.format(excention, len(list(file))))
-    print('Całkowita ilość plików to: ', sum_of_files,'\n')
-    for i in files:
-        print('Model: {}'.format(i) if os.path.splitext(i)[1] == extension_trm else 'Oś: {}'.format(i))
+
+    sunOfFile = 0
+    dicOfExtension = dict()
+    for excentionOfFile, typeOfExtension in it.groupby(files,key= lambda x: os.path.splitext(x)[1]):
+        typeOfExtension = list(typeOfExtension)
+        sunOfFile+=len(typeOfExtension)
+        
+        if excentionOfFile in dicOfExtension:
+            dicOfExtension[excentionOfFile] += len(typeOfExtension)
+        else:
+            dicOfExtension[excentionOfFile] = len(typeOfExtension)
+
+    print('Całkowita ilość plików to: ', sunOfFile,'\n')
+
+    for file in files:
+        print('{}'.format(file))
+
+    for key, value in dicOfExtension.items():
+        print(f"{key} = {value}")
     pyperclip.copy('\n'.join(files))
 
 if __name__ == "__main__":
